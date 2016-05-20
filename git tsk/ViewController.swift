64a03ac4e4402db4ed026588cd0930a044f49c6c
LSN SOFT <lsnsoft@aca10002.ipt.aol.com>
2016-05-20 10:47:38 +0530

    first commit

--- git tsk/ViewController.swift
+++ git tsk/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("first git post")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
